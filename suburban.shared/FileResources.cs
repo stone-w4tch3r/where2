b19acbcda4e8e952642b577c2980992370185f42
dbizimov <d.bizimov@e-promo.ru>
2023-01-12 22:30:55 +0500

    suburban.shared created

---
+++ suburban.shared/FileResources.cs
@@ -0,0 +1,9 @@
+namespace suburban.shared;
+
+public static class FileResources
+{
+    public static class Debug
+    {
+        public static FileInfo FetchedStationsRootDto { get; } = new("fetchedStationsRootDto.json");
+    }
+}
---
+++ suburban.shared/StringResources.cs
@@ -0,0 +1,15 @@
+﻿
+namespace suburban.shared;
+
+public static class StringResources
+{
+    public static class Debug
+    {
+        public const string StationsRootLoadedFromCache = "StationsRoot loaded from cache: [CreationTime: {0}]";
+        public const string DataIsActual = "Data is actual, loading from file";
+    }
+    public static class Exceptions
+    {
+        public const string FetchingAndLoadingFailed = "Failed to fetch data and to load from file";
+    }
+}
