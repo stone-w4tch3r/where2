31f243f5017317b5a1fd06197f1fc4498b441007
dbizimov <d.bizimov@e-promo.ru>
2023-01-24 20:27:02 +0500

    redundant resource deleted

--- suburban.shared/FileResources.cs
+++ suburban.shared/FileResources.cs
@@ -5,7 +5,5 @@ public static class FileResources
     public static class Debug
     {
         public static FileInfo GetFileInfoForFetchedType(Type type) => new($"fetched{type.Name}.json");
-        public static FileInfo FilteredStations { get; } = new("stationsFiltered.json");
-        
     }
 }
