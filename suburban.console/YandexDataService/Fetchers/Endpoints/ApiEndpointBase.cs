029b2bdc5a2d873e6316da9a36e8ed136f2dc966
dbizimov <d.bizimov@e-promo.ru>
2023-01-25 20:34:31 +0500

    added request comment

--- suburban.console/YandexDataService/Fetchers/Endpoints/ApiEndpointBase.cs
+++ suburban.console/YandexDataService/Fetchers/Endpoints/ApiEndpointBase.cs
@@ -6,6 +6,8 @@ namespace suburban.console.YandexDataService.Fetchers.Endpoints;
 public abstract class ApiEndpointBase<TDto> : ApiEndpoint<TDto>
     where TDto : IDto
 {
+    
+    //https://api.rasp.yandex.net/v3.0/schedule/?apikey=741883ec-2d53-4830-aa83-fa17b38c1f66&station=s9607404&transport_types=suburban&limit=999999&result_timezone=europe/moscow
     private const string ApiKeyPathPart = "/?apikey=741883ec-2d53-4830-aa83-fa17b38c1f66";
 
     public override string Path => RootPath + ApiKeyPathPart + string.Join("&", PathParts);
