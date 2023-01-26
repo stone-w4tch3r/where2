9aba7760857cd21abb4e12d3f3b4ecf11914ade1
dbizimov <d.bizimov@e-promo.ru>
2023-01-26 16:11:55 +0500

    small comments fixes

--- suburban.console/DataTypes/Station.cs
+++ suburban.console/DataTypes/Station.cs
@@ -8,9 +8,9 @@ public record Station(
     string Title,
     Codes Codes,
     string? Direction,
-    [property: JsonConverter(typeof(JsonStringEnumConverter))]
+    [property: JsonConverter(typeof(JsonStringEnumConverter))] //todo remove??
     StationType StationType,
     double? Longitude,
-    [property: JsonConverter(typeof(JsonStringEnumConverter))]
+    [property: JsonConverter(typeof(JsonStringEnumConverter))] //todo remove??
     TransportType TransportType,
     double? Latitude) : IDataType;
