574eaca4ee7433f0e22db12a6e648f2b583533f4
d.bizimov <d.bizimov@e-promo.ru>
2023-02-03 12:04:45 +0500

    format fix

--- suburban.console/YandexDataService/Fetchers/DTOs/StationsEndpoint/CodesDto.cs
+++ suburban.console/YandexDataService/Fetchers/DTOs/StationsEndpoint/CodesDto.cs
@@ -3,7 +3,6 @@ using System.Text.Json.Serialization;
 namespace suburban.console.YandexDataService.Fetchers.DTOs.StationsEndpoint;
 
 // ReSharper disable UnusedAutoPropertyAccessor.Global - json deserialization
-
 public record CodesDto : IDto
 {
     [JsonPropertyName("yandex_code")]
