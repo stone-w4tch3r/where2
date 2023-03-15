6e74223994cc0e6cd56b1a0b290a1630351dfdc9
d.bizimov <d.bizimov@e-promo.ru>
2023-03-15 20:59:23 +0500

    tester updated

--- suburban.console.tester/Program.cs
+++ suburban.console.tester/Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Arbus.Network;
+using Arbus.Network.Implementations;
+using suburban.essentials.HelperServices;
+using YandexService.Core.Fetchers;
+using YandexService.Core.Fetchers.DTOs;
+using YandexService.Core.Fetchers.Endpoints;
+
 Console.WriteLine("Hello, World!");
 
-// var fetcher = new DataFetcher<StationScheduleDto, StationScheduleEndpoint>();
+var fetcher = new DataFetcher<StationScheduleEndpoint, StationScheduleDto>(
+    new HttpClientContext(new NativeHttpClient(new WindowsNetworkManager())),
+    new FileService());
+var result = await fetcher.TryFetchData(() => new (new ("s9607404", null)));
+if (result.IsSuccess)
+    Console.WriteLine(result.Value);
+else
+    Console.WriteLine("nope");
