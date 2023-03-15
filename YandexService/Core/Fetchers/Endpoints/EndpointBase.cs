d1a4f009c881009cc5c811784b701a95c402f883
d.bizimov <d.bizimov@e-promo.ru>
2023-03-15 19:42:28 +0500

    path combiner fixed

--- YandexService/Core/Fetchers/Endpoints/EndpointBase.cs
+++ YandexService/Core/Fetchers/Endpoints/EndpointBase.cs
@@ -8,7 +8,7 @@ internal abstract class EndpointBase<TDto> : ApiEndpoint<TDto>
 {
     private const string ApiKeyPathPart = "/?apikey=741883ec-2d53-4830-aa83-fa17b38c1f66";
 
-    public override string Path => RootPath + ApiKeyPathPart + string.Join("&", PathParts);
+    public override string Path => RootPath + ApiKeyPathPart + "&" + string.Join("&", PathParts);
 
     protected abstract string RootPath { get; }
 
