2c787efac2364e8b56e4a0b3ee6e43e21cc8caee
d.bizimov <d.bizimov@e-promo.ru>
2023-03-15 21:13:58 +0500

    function interface deleted

--- YandexService/Core/Fetchers/IDataFetcher.cs
+++
@@ -1,9 +0,0 @@
-using suburban.essentials;
-using YandexService.Core.Fetchers.DTOs;
-
-namespace YandexService.Core.Fetchers;
-
-internal interface IDataFetcher<TDto> where TDto : IDto
-{
-    public Task<Result<TDto>> TryFetchData();
-}
