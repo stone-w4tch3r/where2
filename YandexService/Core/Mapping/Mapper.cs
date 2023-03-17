0cb6801cf0aa1d6d3626f190b4c201db5ede5b02
stone-w4thc3r <no@e.mail>
2023-03-17 22:24:36 +0500

    mapper

--- YandexService/Core/Mapping/Mapper.cs
+++ YandexService/Core/Mapping/Mapper.cs
@@ -1,5 +1,17 @@
+using suburban.essentials;
+using YandexService.API.DataTypes.Abstractions;
+using YandexService.Core.Fetching.DTOs;
+
 namespace YandexService.Core.Mapping;
 
 internal class Mapper
 {
+    public static TModel Map<TDto, TModel>(
+        TDto dto,
+        Func<TDto, TModel> converter,
+        Func<TModel, TModel> filter)
+        where TDto : IDto
+        where TModel : IModel
+        =>
+            converter(dto).Map(filter);
 }
