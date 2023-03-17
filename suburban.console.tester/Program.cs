7a1642f5fcf093a3767aba944964e56f6986a52a
stone-w4thc3r <no@e.mail>
2023-03-17 21:27:27 +0500

    tester commented out

--- suburban.console.tester/Program.cs
+++ suburban.console.tester/Program.cs
@@ -1,17 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Arbus.Network;
-using Arbus.Network.Implementations;
-using suburban.essentials.HelperServices;
-using YandexService.Core.Fetching;
-using YandexService.Core.Fetching.DTOs;
-using YandexService.Core.Fetching.Endpoints;
+// using Arbus.Network;
+// using Arbus.Network.Implementations;
+// using suburban.essentials.HelperServices;
+// using YandexService.Core.Fetching;
+// using YandexService.Core.Fetching.DTOs;
+// using YandexService.Core.Fetching.Endpoints;
 
 Console.WriteLine("Hello, World!");
 
-var fetcher = new Fetcher<StationScheduleEndpoint, StationScheduleDto>(
-    new HttpClientContext(new NativeHttpClient(new WindowsNetworkManager())),
-    new FileService());
-var result = await fetcher.Fetch(() => new(new("s9607404", null)));
-
-Console.WriteLine(result?.ToString() ?? "nope");
+// var fetcher = new Fetcher<StationScheduleEndpoint, StationScheduleDto>(
+//     new HttpClientContext(new NativeHttpClient(new WindowsNetworkManager())),
+//     new FileService());
+// var result = await fetcher.Fetch(() => new(new("s9607404", null)));
+//
+// Console.WriteLine(result?.ToString() ?? "nope");
