fe10f75530293ae795917eb13418143e795edc75
stone-w4thc3r <no@e.mail>
2023-03-18 14:13:13 +0500

    StationsFilter Model -> Dto

--- YandexService/Core/Fetching/Mapping/Filters/StationsFilter.cs
+++ YandexService/Core/Fetching/Mapping/Filters/StationsFilter.cs
@@ -1,20 +1,51 @@
-using YandexService.API.DataTypes;
+using suburban.essentials;
 using YandexService.API.DataTypes.Enums;
+using YandexService.Core.Fetching.DTOs;
 
 namespace YandexService.Core.Fetching.Mapping.Filters;
 
 internal class StationsFilter
 {
-    public static Stations Filter(Stations stations) =>
-        new(stations.Country with
-        {
-            Regions = stations.Country.Regions.Select(region => region with
-            {
-                Settlements = region.Settlements.Select(settlement => settlement with
-                {
-                    Stations = settlement.Stations
-                        .Where(station => station.TransportType is TransportType.Suburban or TransportType.Train)
-                }).Where(settlement => settlement.Stations.Any())
-            }).Where(x => x.Settlements.Any())
-        });
+    private readonly Func<string?, TransportType> _transportTypeConverter;
+
+    public StationsFilter(Func<string?, TransportType> transportTypeConverter)
+    {
+        _transportTypeConverter = transportTypeConverter;
+    }
+
+    public StationsDto Filter(StationsDto stations) =>
+        GetRussia(stations)
+            .MapOrNull(FilterRegionsInCountry)
+            .Map<CountryDto?, StationsDto>(country =>
+                country is not null
+                    ? new(new[] { country })
+                    : new(Enumerable.Empty<CountryDto>()));
+
+    private static CountryDto? GetRussia(StationsDto stations) =>
+        stations.Countries?.FirstOrDefault(country => country.Title == "Россия");
+
+    private CountryDto FilterRegionsInCountry(CountryDto country) =>
+        country with { Regions = GetFilteredRegions(country).Where(HasSettlements) };
+
+    private IEnumerable<RegionDto> GetFilteredRegions(CountryDto country) =>
+        country.Regions?.Select(FilterSettlementsInRegion) ?? Enumerable.Empty<RegionDto>();
+
+    private static bool HasSettlements(RegionDto region) => region.Settlements?.Any() ?? false;
+
+    private RegionDto FilterSettlementsInRegion(RegionDto region) =>
+        region with { Settlements = GetFilteredSettlements(region)?.Where(HasStations) };
+
+    private static bool HasStations(SettlementDto settlement) => settlement.Stations?.Any() ?? false;
+
+    private IEnumerable<SettlementDto>? GetFilteredSettlements(RegionDto region) =>
+        region.Settlements?.Select(FilterStationsInSettlement);
+
+    private SettlementDto FilterStationsInSettlement(SettlementDto settlement) =>
+        settlement with { Stations = FilterStations(settlement.Stations) };
+
+    private IEnumerable<StationDto>? FilterStations(IEnumerable<StationDto>? stations) =>
+        stations?.Where(IsTrainOrSuburban);
+
+    private bool IsTrainOrSuburban(StationDto station) =>
+        _transportTypeConverter(station.TransportType) is TransportType.Train or TransportType.Suburban;
 }
