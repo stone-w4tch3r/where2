68f770b90040ff8b70724901255bec590e32ce67
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2025-05-17 00:25:40 +0500

    eslint and prettierrc

---
+++ backend/eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      // Add or override rules here
+    },
+    settings: {},
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'build/',
+      'coverage/',
+    ],
+  },
+]; 
