aa05fd01d2b88c5aa85953da4365ec559d7c1e72
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2025-05-20 18:10:58 +0500

    use css in js + remove trash files

--- frontend/src/components/LeafletMap/LeafletMap.tsx
+++ frontend/src/components/LeafletMap/LeafletMap.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./LeafletMap.css";
+import styled from "@emotion/styled";
 
 export const MAP_CONTAINER_ID = "map";
 
@@ -7,12 +7,21 @@ export type MapContainerProps = {
   children: React.ReactNode;
 };
 
+const MapContainer = styled.div`
+  height: 100vh; /* Default to full viewport height */
+  width: 100%;
+  background-color: #f0f0f0; /* Light grey background as a visual cue before map tiles load */
+  position: relative; /* For positioning overlays */
+
+  /* Leaflet dynamically adds this class to its container */
+  .leaflet-container {
+    height: 100%;
+    width: 100%;
+  }
+`;
+
 const LeafletMap: React.FC<MapContainerProps> = ({ children }) => {
-  return (
-    <div id={MAP_CONTAINER_ID} className="map-container-dimensions">
-      {children}
-    </div>
-  );
+  return <MapContainer id={MAP_CONTAINER_ID}>{children}</MapContainer>;
 };
 
 export default LeafletMap;
